829a41a2e86818a84c23b150f1a572c395ef1c15
ajoncode <ajoncode@gmail.com>
2021-07-07 19:12:39 +0530

    Storing Focus History With Status

--- App.js
+++ App.js
@@ -7,16 +7,40 @@ import { Timer } from './src/features/timer/Timer';
 import { colors } from './src/utils/colors';
 import { spacing } from './src/utils/sizes';
 
+const STATUSES = {
+  COMPLETE: 1,
+  CANCELLED: 2,
+};
+
 export default function App() {
-  const [focusSubject, setFocusSubject] = React.useState('gardening');
+  const [focusSubject, setFocusSubject] = React.useState(null);
+  const [focusHistory, setFocusHistory] = React.useState([]);
+
+  const addFocusHistorySubjectWithState = (subject, status) => {
+    setFocusHistory([
+      ...focusHistory,
+      {
+        subject,
+        status,
+      },
+    ]);
+  };
+
+  console.info('focusHistory: ', focusHistory);
 
   return (
     <View style={styles.container}>
       {focusSubject ? (
         <Timer
           focusSubject={focusSubject}
-          onTimerEnd={() => { setFocusSubject(null); }}
-          clearSubject={() => setFocusSubject(null)}
+          onTimerEnd={() => {
+            addFocusHistorySubjectWithState(focusSubject, STATUSES.COMPLETE);
+            setFocusSubject(null);
+          }}
+          clearSubject={() => {
+            addFocusHistorySubjectWithState(focusSubject, STATUSES.CANCELLED);
+            setFocusSubject(null);
+          }}
         />
       ) : (
         <Focus addSubject={setFocusSubject} />
